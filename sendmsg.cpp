370f1ec1a4ee5acbc1290cea2b549872b558d974
sensal <sensal@git.com>
2015-12-14 14:44:34 +0800

    add sendmsg() and recvmsg()

---
+++ sendmsg.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+    > File Name: sendmsg.cpp
+    > Author: cocoii
+    > Mail: ys.zhao@helang.com 
+    > Created Time: Mon 14 Dec 2015 11:56:43 AM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+using namespace std;
+
+#define PORT 7777
+
+int main()
+{
+	char buf[1024] = "send buffer through sendmsg";
+	ssize_t size;
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int sendfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct iovec iov[1];
+	iov[0].iov_base = buf;
+	iov[0].iov_len = sizeof(buf);
+
+	struct msghdr msg;
+	msg.msg_name = &addr;
+	msg.msg_namelen = sizeof(addr);
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = 0;
+
+	size = sendmsg(sendfd, &msg, 0);
+}
+

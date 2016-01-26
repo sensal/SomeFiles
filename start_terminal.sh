19b0afa365c1d428d925ba2883c3e9b889f7d055
sensal <sensal@git.com>
2016-01-26 09:08:56 +0800

    add start_terminal.sh

---
+++ start_terminal.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: start_terminal.sh
+# Author: cocoii
+# mail: ys.zhao@helang.com
+# Created Time: Fri 27 Nov 2015 05:02:03 PM CST
+#########################################################################
+#!/bin/bash
+
+`gnome-terminal -x bash -c "./welcom.sh;exec bash;"`
